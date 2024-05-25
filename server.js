c38d5959e1ed53def4a1e21a5ba71cde4ab6f7d3
Atharva <atharva.nv@gmail.com>
2024-05-25 05:04:36 -0700

    install and configure dotenv

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-// Import Express 
+// Import Express to handle routing
 const express = require("express");
 const app = express();
 
+// Import dotenv to hide API key
+require('dotenv').config()
+// api key will be process.env.API_KEY
+
 const port = 3000;
+
+
 
 app.get('/', (req, res) => {
   res.send("Hellsdadsadasd!");
