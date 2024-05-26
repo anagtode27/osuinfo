f2f72bb04b562771455504a296165603a33fff57
Atharva <atharva.nv@gmail.com>
2024-05-26 01:33:59 -0700

    installed more node modules & set up server to send mrekk info at localhost:3000

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 // Import Express to handle routing
-const express = require("express");
-const app = express();
-
-// Import dotenv to hide API key
-require('dotenv').config()
-// api key will be process.env.API_KEY
-
+import express from 'express';
+const app = express(); // app is an instance of Express application
 const port = 3000;
 
+// Import Fetch to make API calls
+import fetch from 'node-fetch';
 
-
-app.get('/', (req, res) => {
-  res.send("Hellsdadsadasd!");
-});
+// Import dotenv to hide API key
+import 'dotenv/config';
+// api key will be process.env.API_KEY
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
+
+
+app.get('/get_user_data', async (req, res) => {
+    const user_api_url = `https://osu.ppy.sh/api/get_user?k=${process.env.API_KEY}&u=mrekk`;
+    const user_fetch_response = await fetch(user_api_url);
+    const user_data = await user_fetch_response.json();
+    console.log(user_data);
+    res.json(user_data);
+});
+
+
