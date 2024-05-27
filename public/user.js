015f1896ce19bf50ce221cd625a1d6b3fdfa5fca
Atharva <atharva.nv@gmail.com>
2024-05-27 04:28:38 -0700

    finally start loading info

--- public/user.js
+++ public/user.js
@@ -34,11 +34,33 @@ const getUserData = async () => {
     const consolidated_data = [data_user, img_b64, data_recent_scores];
     //console.log(consolidated_data);
 
-    populateInfo(consolidated_data);
+    card = document.getElementById("card");
+    populateInfo(consolidated_data, card);
 };
 
-const populateInfo = (data) => {
-     
+const populateInfo = (data, card) => {
+
+    // Doing this directly in the src giving some issues
+    const imgSource = 'data:image/jpeg;base64,' + data[1];
+    
+    card.innerHTML = `
+        <div id="user-card">
+            <img id="pfp_img" src="${imgSource}" alt="Profile Picture">
+            <h2 id="user_name">${data[0][0].username}</h2>
+            <p id="country_origin">${data[0][0].country}</p>
+            <p id="join_date">${data[0][0].join_date}</p>
+            <p id="play_count">Playcount: ${data[0][0].playcount}</p>
+            <p id="global_ranking">Global Ranking: ${data[0][0].pp_rank}</p>
+            <p id="country_ranking">Country Ranking: ${data[0][0].pp_country_rank}</p>
+            <p id="performance_pts">Performance Pts: ${data[0][0].pp_raw}</p>
+            <p id="time_played">Time Played: ${data[0][0].total_seconds_played}</p>
+            <p id="accuracy">Accuracy: ${data[0][0].accuracy}</p>
+            <p id="level">Level: ${data[0][0].level}</p>
+            <div id="chart-container">
+                <canvas id="myChart"></canvas>
+            </div>
+        </div>
+    `;
     const ctx = document.getElementById('myChart');
 
     if (myChart) {
@@ -58,9 +80,12 @@ const populateInfo = (data) => {
       },
       options: {
         scales: {
-            y: [{
+            y: {
                 display: false
-            }]
+            },
+            x: {
+                display: false
+            }
         },
         plugins: {
             title: {
